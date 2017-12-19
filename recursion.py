46abe00d757eb03116696630d5ba8b0fe823e1c0
Rommel Lasso <soccerply@msn.com>
2017-12-19 02:38:16 -0500

    Final Commit

--- recursion.py
+++ recursion.py
@@ -7,3 +7,20 @@ def fibonnaci(n):
         return 1
     else:
         return fibonnaci(n-1) + fibonnaci(n-2)
+
+def gcd(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return gcd(b, (a % b))
+
+def compareTo(s1,s2):
+    if s1<s2:
+        return -1
+    if s1==s2:
+        return 0
+    if s1>s2:
+        return 1
+    return compareTo(s1,s2)
